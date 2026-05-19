0bb54ece2c0a02059030bd9cbc0824dba95d34ab
Srija Rayikanti <srijarayikanti@gmail.com>
2026-05-19 21:44:30 +0530

    openAPI

--- src/main/java/cafe_Management/demo/config/SwaggerConfig.java
+++ src/main/java/cafe_Management/demo/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package cafe_Management.demo.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +17,22 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+
+                // API Information
+                .info(new Info()
+                        .title("Cafe Management System API")
+                        .description("REST APIs for Cafe Management System with JWT Authentication")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Srija")
+                                .email("srijarayikanti@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")))
+
+                // JWT Security
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("BearerAuth"))
+
                 .components(new Components()
                         .addSecuritySchemes("BearerAuth",
                                 new SecurityScheme()
@@ -22,6 +41,11 @@ public class SwaggerConfig {
                                         .scheme("bearer")
                                         .bearerFormat("JWT")
                         )
-                );
+                )
+
+                // External Docs
+                .externalDocs(new ExternalDocumentation()
+                        .description("GitHub Repository")
+                        .url("https://github.com/srijarayikanti"));
     }
 }
